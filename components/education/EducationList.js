0d34e0e9324b4bfb9c68b32d2d7bd4c3629b7671
Gaston Roxas <gacaro51597@gmail.com>
2022-04-19 20:46:49 +0800

    added index category components

--- components/education/EducationList.js
+++ components/education/EducationList.js
@@ -1,15 +1,17 @@
 import EducationItem from "./EducationItem";
 import classes from "./EducationList.module.css";
-import ItemsList from "../ui/ItemsList";
+import ItemsList from "../ui/items-list";
 
 function EducationList(props) {
   const { info } = props;
 
-  return <ul className={classes.list}>
-    {info.map((item) => {
-      return <ItemsList key={item.id} info={info}/>
-    })}
-  </ul>
+  return (
+    <ul className={classes.list}>
+      {info.map((item) => {
+        return <ItemsList key={item.id} info={info} />;
+      })}
+    </ul>
+  );
 }
 
-export default EducationList
+export default EducationList;
