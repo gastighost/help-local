ac81fc386685856a44100fa89a48dfd4f3b4a778
Kang Hsieh <kanghsieh1990@gmail.com>
2022-04-21 17:06:25 +0200

    test setup finished and the two tests for db and index are now working and passing correctly

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,35 +1,11 @@
+/** @jest-environment jsdom */
 import { render } from '@testing-library/react'
+import React from 'react'
 import Home from '../pages/index'
 
-const {MongoClient} = require('mongodb');
-
 describe('insert', () => {
-  let connection;
-  let db;
-
-  beforeAll(async () => {
-    console.log("Test Starting...");
-    connection = await MongoClient.connect(global.__MONGO_URI__, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    db = await connection.db();
-    // await db.collection('COLLECTION_NAME').deleteMany({});
-  });
-
-  afterAll(async () => {
-    console.log("Test Ending...");
-    await connection.close();
-//   if(db.close) {
-//     await db.close();
-//   }
-    console.log("Test ended!");
-  });
-
-  it('renders homepage unchanged', async () => {
+  it('renders homepage unchanged', () => {
     const { container } = render(<Home />)
-    await expect(container).toMatchSnapshot()
-    console.log("rendering ended");
+    expect(container).toMatchSnapshot()
   });
-  console.log("does it get to here?");
 });
