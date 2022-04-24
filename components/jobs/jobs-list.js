7f4c874a845af975e3137dfe010125aa1de2f3fc
Kang Hsieh <kanghsieh1990@gmail.com>
2022-04-24 16:43:35 +0200

    jobs index is now showing jobs read from the database

--- components/jobs/jobs-list.js
+++ components/jobs/jobs-list.js
@@ -1,12 +1,12 @@
 import JobsItem from "./jobs-item";
 
 function jobsList(props) {
-  // console.log(props.jobs);
+  console.log(props.jobs);
   return (
     <ul>
       {props.jobs.map((job) => {
         return (
-          <li key={job.key}>
+          <li key={job._id}>
             <JobsItem job={job} />
           </li>
         );
