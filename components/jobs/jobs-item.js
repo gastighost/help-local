61f1d855653ce060cd2e67fe35c50a73a738b556
Kang Hsieh <kanghsieh1990@gmail.com>
2022-04-25 11:52:48 +0200

    user can now see show page of one single job

--- components/jobs/jobs-item.js
+++ components/jobs/jobs-item.js
@@ -1,6 +1,8 @@
+import Link from "next/link";
 import Card from "../ui/card";
 
 function JobsItem(props) {
+  // console.log(props.job);
   return (
     <Card>
       <h2>Category: {props.job.category}</h2>
@@ -8,6 +10,7 @@ function JobsItem(props) {
       <h3>Company: {props.job.company}</h3>
       <p>Description: {props.job.description}</p>
       <p>Monthly salary in EUR: {props.job.monthlySalary}</p>
+      <Link href={`/jobs/${props.job._id}`}>Open</Link>
     </Card>
   );
 }
