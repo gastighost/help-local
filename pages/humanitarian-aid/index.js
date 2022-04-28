0494484e4dc84dcef0ef1b95a3c96aba09a7a351
Gaston Roxas <gacaro51597@gmail.com>
2022-04-28 10:07:00 +0800

    transformed create humanitarian aid into modal instead of page

--- pages/humanitarian-aid/index.js
+++ pages/humanitarian-aid/index.js
@@ -1,18 +1,32 @@
 import { getAllDocuments } from "../../util/mongodb";
+import { useState } from "react";
 import Link from "next/link";
 import styles from "../../styles/Home.module.css";
 import CategoryFilterBar from "../../components/ui/filter-bar";
 import ItemsList from "../../components/ui/ItemsList";
+import HumanitarianAidForm from "../../components/humanitarian-aid/humanitarian-aid-form";
+import Button from "../../components/ui/button";
 
 function HumanitarianAidIndex(props) {
+  const [newAidModalIsOpen, setNewAidModalIsOpen] = useState(false);
   const { aid } = props;
+
+  const openModal = () => {
+    setNewAidModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setNewAidModalIsOpen(false);
+  };
+
   return (
     <div className={styles.center}>
       <CategoryFilterBar />
+      <div>
+        <Button onClick={openModal}>Create New Aid</Button>
+      </div>
+      {newAidModalIsOpen && <HumanitarianAidForm onCancel={closeModal} />}
       <h1>Humanitarian Aid</h1>
-      <Link href="/humanitarian-aid/new-aid">
-        <a>Create new item</a>
-      </Link>
       <ItemsList info={aid} type="humanitarian-aid" />
       <Link href="/">
         <a>Back to home</a>
--- pages/humanitarian-aid/new-aid.js
+++ pages/humanitarian-aid/new-aid.js
@@ -1,7 +1,18 @@
 import HumanitarianAidForm from "../../components/humanitarian-aid/humanitarian-aid-form";
 import Link from "next/link";
 
-function NewHumanitarianAid() {
+function NewHumanitarianAid(props) {
+  const [modalIsOpened, setModalIsOpened] = useState(true);
+
+  const openModalHandler = () => {
+    setModalIsOpened(true);
+    props.modalState;
+  };
+
+  const closeModalHandler = () => {
+    setModalIsOpened(false);
+    props.modalState;
+  };
   return (
     <div>
       <HumanitarianAidForm />
