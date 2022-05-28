720ec632b20e214dcc2d455f77a94b5246552395
Stella Raab <hallo@stellaraab.de>
2022-05-28 14:02:54 +0200

    Set up social media icon, styling will follow

--- components/social-media/cardcontainer.js
+++ components/social-media/cardcontainer.js
@@ -8,11 +8,16 @@ function Cardcontainer() {
         <div className={styles.socialimage}>
           <img></img>
         </div>
-        <div className={styles.cardinfo}>
-          <h4 className={styles.cardtitle}>Support group for Ukrainian refugees what is this!</h4>
-          <div className={styles.cardbottom}>
-            <div className={styles.icontext}><LockOutlinedIcon /><p>private</p></div>
-            <div className={styles.members}><p><strong>12345</strong> members</p></div>
+        <div className={styles.contentright}>
+          <div className={styles.upperpart}>
+            <h4 className={styles.cardtitle}>Support group for Ukrainian refugees what is this!</h4>
+            <div className={styles.cardbottom}>
+              <div className={styles.icontext}><LockOutlinedIcon /><p>private</p></div>
+              <div className={styles.members}><p><strong>12345</strong> members</p></div>
+            </div>
+          </div>
+          <div className={styles.socialcategory}>
+            <img src='/social-facebook.png' alt='Help Local Icon' className={styles.socialicon}/>
           </div>
         </div>
       </div>
